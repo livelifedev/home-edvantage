b16e8bb6a3f9f6dea04033914b35561fe6d2d8eb
John <johnrubio93@gmail.com>
2020-12-06 00:54:50 +1100

    update courses page

--- pages/categories/index.js
+++ pages/categories/index.js
@@ -3,26 +3,38 @@ import {
   Box,
   Button,
   Container,
+  Flex,
   Heading,
   Icon,
   Input,
   InputGroup,
   InputLeftElement,
   Link,
-  SimpleGrid,
+  Text,
+  VStack,
 } from "@chakra-ui/react";
 import { BsSearch } from "react-icons/bs";
 import { CATEGORIES } from "../../src/utils/constants";
 
 function CategoryCard({ name, href }) {
   return (
-    <Box borderWidth="1px" borderRadius="md" overflow="hidden">
+    <Box borderWidth="1px" borderRadius="md" overflow="hidden" w="full">
       <NextLink href={`/categories/${href}`} passHref>
         <Link color="blue.600">
-          <Box bg="gray.200" w="full" h="48" />
-          <Heading as="h5" size="sm" align="center" py="4" px="2">
-            {name}
-          </Heading>
+          <Flex>
+            <Box bg="gray.200" h="72" w="80" flexShrink="0" />
+            <Box py="8" px="4">
+              <Heading as="h3" size="lg" pb="4">
+                {name}
+              </Heading>
+              <Text color="gray.500">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
+              </Text>
+            </Box>
+          </Flex>
         </Link>
       </NextLink>
     </Box>
@@ -50,11 +62,11 @@ export default function Home() {
             />
             <Input type="search" placeholder="Search all courses" />
           </InputGroup>
-          <SimpleGrid columns={[1, 2, 3, 4]} spacing="4" w="full">
+          <VStack spacing="4" w="full">
             {CATEGORIES.map((x) => (
               <CategoryCard name={x.name} href={x.id} key={x.id} />
             ))}
-          </SimpleGrid>
+          </VStack>
         </Container>
       </Box>
     </main>
