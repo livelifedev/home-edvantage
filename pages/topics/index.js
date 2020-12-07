63ecb6e10b6de7166545b84adcd2d888432e96a7
John <johnrubio93@gmail.com>
2020-12-07 21:26:17 +1100

    added topic descriptions

--- pages/topics/index.js
+++ pages/topics/index.js
@@ -16,12 +16,12 @@ import {
 import { BsSearch } from "react-icons/bs";
 import { CATEGORIES } from "../../src/utils/constants";
 
-function CategoryCard({ name, href }) {
+function CategoryCard({ name, href, description }) {
   return (
     <Box borderWidth="1px" borderRadius="md" overflow="hidden" w="full">
       <Flex direction={["column", null, "row"]}>
-        <Box bg="gray.200" h="72" w={["full", null, 80]} flexShrink="0" />
-        <Box py="8" px="4" pos="relative" h="72">
+        <Box bg="gray.200" h="60" w={["full", null, 80]} flexShrink="0" />
+        <Box py="8" px="4" pos="relative" h="60">
           <NextLink href={`/topics/${href}`} passHref>
             <Link color="blue.600">
               <Heading as="h3" size="lg" pb="4">
@@ -29,11 +29,7 @@ function CategoryCard({ name, href }) {
               </Heading>
             </Link>
           </NextLink>
-          <Text color="gray.500">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s.
-          </Text>
+          <Text color="gray.500">{description}</Text>
           <NextLink href={`/topics/${href}`} passHref>
             <Button colorScheme="blue" pos="absolute" bottom="8" as="a">
               Browse Courses
@@ -68,7 +64,12 @@ export default function Home() {
           </InputGroup>
           <VStack spacing="4" w="full">
             {CATEGORIES.map((x) => (
-              <CategoryCard name={x.name} href={x.id} key={x.id} />
+              <CategoryCard
+                name={x.name}
+                href={x.id}
+                key={x.id}
+                description={x.description}
+              />
             ))}
           </VStack>
         </Container>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,12 +1,52 @@
 export const CATEGORIES = [
-  { name: "Basic Algebra Skills", id: "basic-alegebra-skills" },
-  { name: "Word Problem Skills", id: "word-problem-solving-skills" },
-  { name: "Solving Equations", id: "solving-equations" },
-  { name: "Sketching Functions", id: "sketching-functions" },
-  { name: "Learning Diagrams", id: "learning-diagrams" },
-  { name: "Understanding Calculus", id: "understanding-calculus" },
-  { name: "Selective Placement Training", id: "selective-placment-training" },
-  { name: "Opportunity Class Training", id: "opportunity-class-training" },
+  {
+    name: "Basic Algebra Skills",
+    id: "basic-alegebra-skills",
+    description:
+      "Learning the core basics to working with algebra and the easiest ways to solve questions.",
+  },
+  {
+    name: "Word Problem Skills",
+    id: "word-problem-solving-skills",
+    description:
+      "Find the key steps and skills in solving word problems and the easiest way to get the answer.",
+  },
+  {
+    name: "Solving Equations",
+    id: "solving-equations",
+    description:
+      "Learn the fundamental techniques in solving unknowns in any given equation. ",
+  },
+  {
+    name: "Sketching Functions",
+    id: "sketching-functions",
+    description:
+      "We teach you the easiest steps and skills you need to draw any function or curve you want!",
+  },
+  {
+    name: "Learning Diagrams",
+    id: "learning-diagrams",
+    description:
+      "We show you how to understand and use various graphs, tables and diagrams through various topics.",
+  },
+  {
+    name: "Understanding Calculus",
+    id: "understanding-calculus",
+    description:
+      "Get the right head start you need from now til the end. Calculus doesn’t have to be as hard as it sound.",
+  },
+  {
+    name: "Selective Placement Training",
+    id: "selective-placment-training",
+    description:
+      "We provide free and premium training for Year 5 and 6 students that undergo the Selective School Exam.",
+  },
+  {
+    name: "Opportunity Class Training",
+    id: "opportunity-class-training",
+    description:
+      "We also give free and premium training for Year 3 and 4 students that applied for the O.C. Placement Exam.",
+  },
 ];
 
 export const COURSES = Array.from({ length: 5 }, (v, i) => ({
