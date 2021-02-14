868e5c6d82aac4b22aa76b1141accebd957c357a
John <johnrubio93@gmail.com>
2021-02-14 21:02:44 +1100

    fix graphql endpoint

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://he-cms.herokuapp.com/api/graphql`;
+export const prodEndpoint = `https://he-cms.herokuapp.com/admin/api`;
 export const perPage = 4;
